a439b151d0dfca8b76a633946f2d6c21c59d4ab2
ARoman87 <angel427roman@gmail.com>
2022-12-01 20:54:49 -0500

    Added useState for image generation.

--- memeGenerator/src/components/meme.jsx
+++ memeGenerator/src/components/meme.jsx
@@ -1,13 +1,16 @@
 import memesData from "../memesData"
+import React from "react"
 
 function Meme() {
+    const [memeImage, setMemeImage] = React.useState("")
 
     function getMemeImage() {
         const memesArray = memesData.data.memes 
         const randomNumber = Math.floor(Math.random() * memesArray.length)
         const url = memesArray[randomNumber].url
-        console.log(url)
+        setMemeImage(url)
     }
+
 
 
     return (
@@ -18,6 +21,10 @@ function Meme() {
             </div>
             <div className="memeButton">
                 <button onClick={getMemeImage}>Get a new meme image 🖼️</button>
+            </div>
+
+            <div className="memeImageContainer">
+                <img src={memeImage} alt="" />
             </div>
         </div>
     )
